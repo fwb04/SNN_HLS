987bc40188afb660862395f1e66406bb76162dc5
Fang Wenbing <48020828+fwb04@users.noreply.github.com>
2022-06-30 12:05:57 +0800

    modify datatype

--- snn.h
+++ snn.h
@@ -1,4 +1,16 @@
 #include "iostream"
 // #include "weights.h"
 
-void SNN_infer(uint1_t ecg[260], uint1_t* result);
+
+#ifndef uint1_t
+#include <ap_int.h>
+typedef ap_uint<1> uint1_t;
+#endif
+
+#ifndef float24_t
+#include <ap_fixed.h>
+// typedef float float24_t;
+typedef ap_fixed<24, 6, AP_RND, AP_SAT> float24_t;
+#endif
+
+void SNN_infer(uint1_t ecg[260], int *result);
